02bc4fbf7b9e0a233080a7c99615336b3b353a41
Houser97 <arturo.riverar97@gmail.com>
2022-08-02 18:34:26 -0500

    Create mongoDB cluster and link it to the project

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const mongoose = require("mongoose");
+let mongoDB = 'mongodb+srv://arturo:houser@cluster0.hidb3so.mongodb.net/inventory-app?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
