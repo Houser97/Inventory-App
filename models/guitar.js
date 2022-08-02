2aa58907caf46b750200c02a3d4f8fdd918c53fe
Houser97 <arturo.riverar97@gmail.com>
2022-08-02 18:56:53 -0500

    Create guitar model

---
+++ models/guitar.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const guitarSchema = new Schema({
+    name: {type: String, required:true, maxLength: 100},
+    description: {type: String, required: true, maxLength: 100},
+    category: {type: Schema.Types.ObjectId, ref: "Type", required: true},
+    price: {type: Number, required: true},
+    number_in_stock: {type: Number},
+    brand: {type: Schema.Types.ObjectId, ref: "Brand", required: true},
+})
+
+//Propiedades virtuales
+guitarSchema.virtual('url').get(function(){
+    return '/category/guitar' + this._id;
+})
+
+module.exports = mongoose.model("Guitar", guitarSchema);
