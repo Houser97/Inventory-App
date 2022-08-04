27f63d7b6f2dd1473f3a18f5084deff01ac9f427
Houser97 <arturo.riverar97@gmail.com>
2022-08-04 17:01:28 -0500

    Add functionality to add new type objects
    These objects refer to the physical phenomena used by the intruments to
    generate the sound. In this commit the style and the form for the object
    are also added, which are the same as those used for the creation of the
    brands.

---
+++ controllers/typeController.js
@@ -0,0 +1,37 @@
+const Type = require('../models/type');
+const { body, validationResult } = require('express-validator');
+
+// Petición para crear type, GET
+exports.type_create_get = function(req, res, next){
+    res.render('type_form', {
+        title: "Create Type"
+    });
+}
+
+// Petición para crear type, POST
+exports.type_create_post = [
+    body('type', 'Type must be specified').trim().isLength({min: 3}).escape(),
+    body('description', 'Description must not be empty').trim().isLength({min: 3}).escape(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        const type = new Type({
+            name: req.body.type,
+            description: req.body.description,
+        });
+
+        if(!errors.isEmpty()){
+            res.render('type_form', {
+                title: 'Create Type',
+                type: type,
+                errors: errors.array(),
+            });
+            return;
+        } else {
+            type.save(function(err){
+                if(err){ return next(err); }
+                res.redirect('/')
+            });
+        }
+    }
+];
--- routes/category.js
+++ routes/category.js
@@ -3,6 +3,7 @@ let router = express.Router();
 
 const category_controller = require('../controllers/categoryController');
 const brand_controller = require('../controllers/brandController');
+const type_controller = require('../controllers/typeController');
 
 /// --------- Rutas para cada categoría --------- ///
 
@@ -17,5 +18,13 @@ router.get('/brand/create', brand_controller.brand_create_get);
 
 // Crear Brand, POST
 router.post('/brand/create', brand_controller.brand_create_post);
+
+
+//------------ Type
+//Crear Type, GET
+router.get('/type/create', type_controller.type_create_get);
+
+// Crear Brand, POST
+router.post('/type/create', type_controller.type_create_post);
 
 module.exports = router;
